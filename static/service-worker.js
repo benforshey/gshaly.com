30b492ea17cc70238220f36bf00434468af06e40
Ben Forshey <ben@integrisweb.com>
2022-04-25 18:29:22 +0100

    Push for Manual Testing

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-env worker */
 const cacheName = "offlineFirst";
-const cacheVersion = "v0.0.15_dev-001";
+const cacheVersion = "v0.0.15_dev-002";
 const cacheURIs = [
   // If any file in this list fails, the whole service worker fails to install.
   // Pages
