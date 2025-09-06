87ad39d0934a58d9e46bb17b16b2c33a9378b3a1
Ben Forshey <ben@integrisweb.com>
2025-09-06 14:32:26 -0400

    feat: update service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-env worker */
 const cacheName = "offlineFirst";
-const cacheVersion = "v0.0.17";
+const cacheVersion = "v0.0.18";
 const cacheURIs = [
   // If any file in this list fails, the whole service worker fails to install.
   // Pages
