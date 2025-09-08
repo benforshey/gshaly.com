8f00e934b416ff38e582702a8bb78399cf5295b2
Ben Forshey <ben@integrisweb.com>
2025-09-08 18:42:29 -0400

    feat: update slideshow

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-env worker */
 const cacheName = "offlineFirst";
-const cacheVersion = "v0.0.18";
+const cacheVersion = "v0.0.19";
 const cacheURIs = [
   // If any file in this list fails, the whole service worker fails to install.
   // Pages
@@ -32,7 +32,7 @@ const cacheURIs = [
   "/tea-knowledge/matching-a-standard/",
   "/404.html",
   // Style
-  "/dist/style/main_v1.0.2.css",
+  "/dist/style/main_v1.0.3.css",
   // Script
   "/dist/script/bundle_v1.0.2.js",
   // Images
