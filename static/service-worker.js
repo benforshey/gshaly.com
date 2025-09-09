67f45e28a8f78d2f9af848ee1f30b8929ed44124
Ben Forshey <ben@integrisweb.com>
2025-09-08 21:07:58 -0400

    feat: update timeline slideshow

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-env worker */
 const cacheName = "offlineFirst";
-const cacheVersion = "v0.0.19";
+const cacheVersion = "v0.0.20";
 const cacheURIs = [
   // If any file in this list fails, the whole service worker fails to install.
   // Pages
